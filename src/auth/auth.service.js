a8d87709a537b46443c23e873f2ab6fd7b3b606a
tapan176 <tapankhokhariya176@gmail.com>
2023-08-29 18:32:46 +0530

    updates

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -7,7 +7,7 @@ module.exports = {
         try {
             const queryResult = await dbClient.query(`SELECT "email", "passwordHash" FROM users WHERE "email" = '${email}'`);
             if (queryResult.rowCount > 0) {
-                const passwordMatch = await bcrypt.compare(password, queryResult.row[0].passwordHash);
+                const passwordMatch = await bcrypt.compare(password, queryResult.rows[0].passwordHash);
                 if (passwordMatch) {
                     return {message: "login successful", code: "login_successful"};
                 } else {
@@ -26,7 +26,7 @@ module.exports = {
         try {
             if (password === confirmPassword) {
                 const hashedPassword = await bcrypt.hash(password, 10);
-                // const queryResult = await dbClient.query(`INSERT INTO users ("firstName", "lastName", "email", "password") VALUES ('${firstName}', '${lastName}', '${email}', '${passwordHash}')`);
+                const queryResult = await dbClient.query(`INSERT INTO users ("firstName", "lastName", "email", "password") VALUES ('${firstName}', '${lastName}', '${email}', '${passwordHash}')`);
                 return hashedPassword;
             }
         } finally {
